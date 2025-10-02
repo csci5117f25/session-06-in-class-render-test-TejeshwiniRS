eecd3f8d57484425ea6c2fe92032f40ffbcd9eec
TejeshwiniRS <tejuswetha20@gmail.com>
2025-10-02 09:15:05 -0500

    [AUTH0] Cofigured Login logout and callback

--- server.py
+++ server.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import psycopg2
 import os
+# Auth0 imports
+from urllib.parse import quote_plus, urlencode
+
+
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+load_dotenv()
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
@@ -11,6 +18,46 @@ cursor = conn.cursor()
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('FLASK_SECRET')
+###AUTH STUFF ###
+
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=os.environ.get("AUTH0_CLIENT_ID"),
+    client_secret=os.environ.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{os.environ.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
+
+@app.route('/login/')
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect(url_for('showList'))
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://" + os.environ.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("showList", _external=True),
+                "client_id": os.environ.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
 
 
 def get_db_connection():
@@ -36,7 +83,7 @@ def showList():
     name = None
     if "name" in session:
         name = session["name"]
-    return render_template('hello.html', guests=guests, name=name)
+    return render_template('hello.html', guests=guests, name=name, session=session.get('user'))
 
 @app.route('/display')
 def display():
